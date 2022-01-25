df7ae11f8dd7aaac931cbf5fc5b4918c5f3f0e88
Max Pavlov <ifelseapps@gmail.com>
2022-01-25 09:16:06 +0300

    Сортировка временной ленты

--- apps/jewishhistory.info/components/Timeline/Timeline.tsx
+++ apps/jewishhistory.info/components/Timeline/Timeline.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { ITimelineItem, Timeline as TimelineType } from 'types/Timeline';
 import Link from 'next/link';
 import { getEntityUrl } from '../../utils/entity';
@@ -19,7 +19,46 @@ interface ITimelineProps {
   timeline: TimelineType;
 }
 
-export const Timeline: FC<ITimelineProps> = ({ timeline }) => {
+export const Timeline: FC<ITimelineProps> = (props) => {
+  const [timeline, setTimeline] = useState<TimelineType>(props.timeline);
+
+  const sortByAsc = useCallback(
+    () => {
+      const updated = props.timeline.slice();
+
+      updated.forEach((item) => {
+        item.events = item.events
+          .slice()
+          .sort((src, dest) => src.dateStart - dest.dateStart);
+      });
+
+      // Отсортируем эпохи в порядке возрастания по первому событию в эпохе
+      updated.sort((src, dest) => (src.events[0] || { dateStart: 0 }).dateStart - (dest.events[0] || { dateStart: 0 }).dateStart);
+
+      setTimeline(updated);
+    },
+    []
+  );
+
+  const sortByDesc = useCallback(
+    () => {
+      const updated = props.timeline.slice();
+
+      updated.forEach((item) => {
+        item.events = item.events
+          .slice()
+          .sort((src, dest) => dest.dateStart - src.dateStart);
+
+      });
+
+      // Отсортируем эпохи в порядке возрастания по первому событию в эпохе
+      updated.sort((src, dest) => (dest.events[0] || { dateStart: 0 }).dateStart - (src.events[0] || { dateStart: 0 }).dateStart);
+
+      setTimeline(updated);
+    },
+    []
+  );
+
   const renderPersons = (era: ITimelineItem) => {
     if (!era.persons.length) {
       return null;
@@ -72,20 +111,26 @@ export const Timeline: FC<ITimelineProps> = ({ timeline }) => {
   };
 
   return (
-    <ul className={styles.timeline}>
-      {timeline.map(era => (
-        <li key={era.code} className={styles.item}>
-          <div className={styles.itemTitle}>
-            <Link href={getEntityUrl(era)}>
-              <a>
-                {era.name}
-              </a>
-            </Link>
-          </div>
-          {renderPersons(era)}
-          {renderEvents(era)}
-        </li>
-      ))}
-    </ul>
+    <>
+      <div>
+        <button onClick={sortByAsc}>По возрастанию</button>
+        <button onClick={sortByDesc}>По убыванию</button>
+      </div>
+      <ul className={styles.timeline}>
+        {timeline.map(era => (
+          <li key={era.code} className={styles.item}>
+            <div className={styles.itemTitle}>
+              <Link href={getEntityUrl(era)}>
+                <a>
+                  {era.name}
+                </a>
+              </Link>
+            </div>
+            {renderPersons(era)}
+            {renderEvents(era)}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
